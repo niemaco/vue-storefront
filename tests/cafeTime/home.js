0b4a6e46f6d7a7ab16ac4fdf47f8b67c678491d6
Karol Osuchowski <karol.osuchowski@snow.dog>
2018-08-31 13:45:34 +0200

    Add first test for home page

---
+++ tests/cafeTime/home.js
@@ -0,0 +1,13 @@
+import { Selector } from 'testcafe'
+
+fixture `Getting Started`
+  .page `http://localhost:3000`
+
+test('My first test', async t => {
+  const menuButton = Selector('.bg-cl-transparent.brdr-none.inline-flex.p15.icon.bg-cl-secondary.pointer')
+  const sidebarMenu = Selector('.sidebar-menu')
+
+  await t
+    .click(menuButton)
+    .expect(sidebarMenu.withAttribute('class', 'active'))
+})
